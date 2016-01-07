e44c99eb2be18b97eec512ef897f5aacfa9640ce
Prayag Verma <prayag.verma@gmail.com>
2016-01-07 23:31:57 +0530

    Intial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+** How to Run the Project
+
+1. Do ``npm install``
+2. Do ``gulp build``
---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var inject = require('gulp-inject'),
+	gulp = require('gulp'),
+    path = require('path');
+
+gulp.task('build', function () {
+	views();
+});
+gulp.task('views', views);
+function views() {
+        gulp.src(path.normalize('./frontend/src/*.js'))
+        .pipe(gulp.dest(path.normalize('./build/static')));
+		gulp.src(path.normalize('./app/index.html'))
+	    .pipe(inject(
+            gulp.src([path.normalize('./frontend/src/*.js')], {read: false}), {
+                transform : function ( filePath, file, i, length ) {
+                     var newPath = filePath.replace('/frontend/src', '');
+                     console.log('inject script = '+ newPath);
+                     return '<script src="/static' + newPath  + '"></script>';
+                }
+            }
+        ))
+        .pipe(gulp.dest('./build'));
+}
